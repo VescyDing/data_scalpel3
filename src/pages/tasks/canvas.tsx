b7a1af7e71d99f6b10585c91459e8cb9d9ba3dfd
丁文星 <2557544773@qq.com>
2024-09-09 15:25:51 +0800

    [feat] 弃用垃圾reactflow

--- src/pages/tasks/canvas.tsx
+++ src/pages/tasks/canvas.tsx
@@ -23,9 +23,7 @@ import { history } from '@umijs/max';
 
 export default () => {
     return <PageContainer title='任务配置' breadcrumb={<Breadcrumb className='ant-page-header-breadcrumb' routes={[{ breadcrumbName: '数据汇聚' }, { breadcrumbName: '任务配置' }]} />} >
-        {/* return <PageContainer > */}
-        <div className='white-block' style={{ width: '100%', height: 'calc(100vh - 206px)' }}>
-
+        <div style={{ width: '100%', padding: 0, height: 'calc(100vh - 206px)' }}>
         </div>
     </PageContainer>
 }
